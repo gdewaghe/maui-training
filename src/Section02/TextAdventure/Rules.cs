c8094608ca573838ce815677b1a4cc2ed3a53a3e
gdewaghe <guillaume.dewaghe@gmail.com>
2026-03-14 08:00:01 +0100

    Add Rules class

---
+++ src/Section02/TextAdventure/Rules.cs
@@ -0,0 +1,13 @@
+﻿namespace TextAdventure;
+
+internal static class Rules
+{
+    public static void Display()
+    {
+        Console.WriteLine("Welcome to the Text Adventure Game!");
+        Console.WriteLine("Your goal is to go to the castle and save the princess!");
+        Console.WriteLine("You will make choices that will affect the outcome of your adventure.");
+        Console.WriteLine("Good luck, and have fun playing!");
+        Console.WriteLine();
+    }
+}
