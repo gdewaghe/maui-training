00f70be6a302f6afe8323fdcbd52f286f7088f5a
gdewaghe <guillaume.dewaghe@gmail.com>
2026-03-14 08:46:11 +0100

    Add `DisplayChoice()` and refactor `FirstChoice()` in `Choices`

--- src/Section02/TextAdventure/Choices.cs
+++ src/Section02/TextAdventure/Choices.cs
@@ -4,24 +4,39 @@ internal static class Choices
 {
     public static void FirstChoice()
     {
+        DisplayChoice(
+            "You are in the forest. You can go either to the east or west.",
+            "East",
+            "West",
+            "You exit the forest and see the castle.",
+            "You fall off a cliff and die.");
+    }
+
+    private static void DisplayChoice(
+        string choiceMessage,
+        string firstChoice,
+        string secondChoice,
+        string validChoiceMessage,
+        string invalidChoiceMessage)
+    {
         bool validChoice = false;
-        Console.WriteLine("You are in the forest. You can go either to the east or west.");
+        Console.WriteLine(choiceMessage);
         while (!validChoice)
         {
             Console.WriteLine("What do you choose?");
-            Console.WriteLine("1. East");
-            Console.WriteLine("2. West");
+            Console.WriteLine($"1. {firstChoice}");
+            Console.WriteLine($"2. {secondChoice}");
             ConsoleKey key = Console.ReadKey().Key;
             Console.WriteLine();
             switch (key)
             {
                 case ConsoleKey.D1 or ConsoleKey.NumPad1:
-                    Console.WriteLine("You fall off a cliff and die.");
+                    Console.WriteLine(invalidChoiceMessage);
                     Console.WriteLine("Game Over.");
                     Environment.Exit(0);
                     break;
                 case ConsoleKey.D2 or ConsoleKey.NumPad2:
-                    Console.WriteLine("You exit the forest and see the castle.\n");
+                    Console.WriteLine($"{validChoiceMessage}\n");
                     validChoice = true;
                     break;
                 default:
