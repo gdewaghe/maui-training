74fde22fac0ffe3606a7885766c98db6ab4a3285
gdewaghe <guillaume.dewaghe@gmail.com>
2026-03-20 22:04:31 +0100

    Trainings: Remove underline from Entry control on Android

--- src/Section06/Trainings/Platforms/Android/MainApplication.cs
+++ src/Section06/Trainings/Platforms/Android/MainApplication.cs
@@ -1,10 +1,22 @@
 ﻿using Android.App;
+using Android.Content.Res;
 using Android.Runtime;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
 
 namespace Trainings.Platforms.Android;
 
 [Application]
 public class MainApplication(IntPtr handle, JniHandleOwnership ownership) : MauiApplication(handle, ownership)
 {
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp()
+    {
+        // Remove Entry control underline
+        EntryHandler.Mapper.AppendToMapping("NoUnderline", (handler, entry) =>
+        {
+            handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
+        });
+
+        return MauiProgram.CreateMauiApp();
+    }
 }
