a7e535dbe0a14a2add3f9bc4682ecbdd0a949a78
gdewaghe <guillaume.dewaghe@gmail.com>
2026-03-22 18:34:36 +0100

    TrainingsAPI: Replace WeatherForecast API with Category API

---
+++ src/Section06/TrainingsAPI/Category.cs
@@ -0,0 +1,12 @@
+namespace TrainingsAPI;
+
+public class Category(string title, int count, string description, string image)
+{
+    public string Title { get; set; } = title;
+
+    public int Count { get; set; } = count;
+
+    public string Description { get; set; } = description;
+
+    public string Image { get; set; } = image;
+}
