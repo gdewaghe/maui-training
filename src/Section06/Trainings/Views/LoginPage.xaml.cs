8d3c0d7793fd2b3addc14389ee1853426e79babc
gdewaghe <guillaume.dewaghe@gmail.com>
2026-04-01 21:51:49 +0200

    Trainings: Add login in API logic

--- src/Section06/Trainings/Views/LoginPage.xaml.cs
+++ src/Section06/Trainings/Views/LoginPage.xaml.cs
@@ -2,13 +2,36 @@ namespace Trainings.Views;
 
 public partial class LoginPage : ContentPage
 {
-	public LoginPage()
-	{
-		InitializeComponent();
-	}
-
-    private void OnLogInButtonClicked(object? sender, EventArgs e)
+    public LoginPage()
     {
+        InitializeComponent();
+    }
 
+    private async void OnLogInButtonClicked(object? sender, EventArgs e)
+    {
+        // To be used on local
+        //if (usernameEntry.Text == "guillaume" && passwordEntry.Text == "123")
+        //{
+        //    return true;
+        //}
+
+        // http://10.0.2.2:5152/Login?username=guillaume&password=123
+        var restURL = $"http://10.0.2.2:5152/Login?username={usernameEntry.Text}&password={passwordEntry.Text}";
+        var client = new HttpClient
+        {
+            BaseAddress = new Uri(restURL)
+        };
+
+        HttpResponseMessage response = await client.GetAsync(restURL);
+        var content = await response.Content.ReadAsStringAsync();
+
+        if (content == usernameEntry.Text)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+        else
+        {
+            // TODO: Error
+        }
     }
 }
