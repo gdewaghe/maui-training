0c4fdbcf78a840cae58b3eddcae9cf7d437cbd8b
gdewaghe <guillaume.dewaghe@gmail.com>
2026-04-05 21:04:20 +0200

    feat(taking-notes): add project

---
+++ src/Section07/TakingNotes/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TakingNotes;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
