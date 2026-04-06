d4eecd502f496a58d03032e7e9e6eb02c40a25ca
gdewaghe <guillaume.dewaghe@gmail.com>
2026-04-06 14:28:35 +0200

    feat: add about page

---
+++ src/Section07/TakingNotes/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace TakingNotes.Views;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private void OnLearnMoreClicked(object? sender, EventArgs e)
+    {
+
+    }
+}
