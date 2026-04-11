2139b92662ec4c4ea58854a33f3a4527b75d564d
Guillaume DEWAGHE <55316421+gdewaghe@users.noreply.github.com>
2026-04-11 20:17:41 +0200

    feat: add `Mvvm` project

---
+++ src/Section08/Mvvm/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Mvvm;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/Section08/Mvvm/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Mvvm.ViewModels;
+
+namespace Mvvm.Views;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage(MainViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+}
