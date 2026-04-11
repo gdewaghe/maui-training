9cd98bdfa808ad9b4ce0ec3e943081990e51d36a
gdewaghe <guillaume.dewaghe@gmail.com>
2026-04-11 20:08:30 +0200

    feat(mvvm): add view-model for the main page

---
+++ src/Section08/Mvvm/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace Mvvm.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _text;
+
+    [ObservableProperty]
+    private ObservableCollection<string> _ingredients;
+
+    public MainViewModel()
+    {
+        Ingredients = [];
+    }
+
+    [RelayCommand]
+    private void AddItem()
+    {
+        Ingredients.Add(Text);
+        Text = string.Empty;
+    }
+}
