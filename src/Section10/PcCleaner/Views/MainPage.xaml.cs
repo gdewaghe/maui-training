fee2df2c48c7b18b33808537e0be73b4f92bc9ab
gdewaghe <guillaume.dewaghe@gmail.com>
2026-04-19 15:42:47 +0200

    feat(main-page): add top menu content and logic

--- src/Section10/PcCleaner/Views/MainPage.xaml.cs
+++ src/Section10/PcCleaner/Views/MainPage.xaml.cs
@@ -6,4 +6,17 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
     }
+
+    private async void OnInfoClicked(object? sender, EventArgs e)
+    {
+        try
+        {
+            Uri uri = new("http://anthony-cardinale.fr?from=pccleaner");
+            await Browser.Default.OpenAsync(uri);
+        }
+        catch (Exception ex)
+        {
+            //An unexpected error occurred. No browser may be installed on this device
+        }
+    }
 }
