0d1198f79d06f865cefb858eec1960cae912cc5f
gdewaghe <guillaume.dewaghe@gmail.com>
2026-05-05 11:14:02 +0200

    feat(pc-cleaner): add debug label and checkbox events

--- src/Section10/PcCleaner/Views/MainPage.xaml.cs
+++ src/Section10/PcCleaner/Views/MainPage.xaml.cs
@@ -4,6 +4,12 @@ namespace PcCleaner.Views;
 
 public partial class MainPage : ContentPage
 {
+    private bool _temporaryFilesChecked = true;
+    private bool _binChecked = true;
+    private bool _logsChecked = true;
+    private bool _windowsUpdateChecked = true;
+    private bool _errorsChecked = true;
+
     public MainPage()
     {
         InitializeComponent();
@@ -28,5 +34,40 @@ public partial class MainPage : ContentPage
         {
             //An unexpected error occurred. No browser may be installed on this device
         }
+    }
+
+    private void OnCleanClicked(object? sender, EventArgs e)
+    {
+        DebugLabel.Text = $"Info: " +
+            $"{_temporaryFilesChecked} - " +
+            $"{_binChecked} - " +
+            $"{_logsChecked} - " +
+            $"{_windowsUpdateChecked} - " +
+            $"{_errorsChecked}";
+    }
+
+    private void OnTemporaryFilesCheckedChanged(object? sender, CheckedChangedEventArgs e)
+    {
+        _temporaryFilesChecked = e.Value;
+    }
+
+    private void OnEmptyBinCheckedChanged(object? sender, CheckedChangedEventArgs e)
+    {
+        _binChecked = e.Value;
+    }
+
+    private void OnLogsCheckedChanged(object? sender, CheckedChangedEventArgs e)
+    {
+        _logsChecked = e.Value;
+    }
+
+    private void OnWindowsUpdateCheckedChanged(object? sender, CheckedChangedEventArgs e)
+    {
+        _windowsUpdateChecked = e.Value;
+    }
+
+    private void OnErrorsCheckedChanged(object? sender, CheckedChangedEventArgs e)
+    {
+        _errorsChecked = e.Value;
     }
 }
