6a23ef6df6d74bf29f905d99063875aba587da9a
gdewaghe <guillaume.dewaghe@gmail.com>
2026-05-05 12:03:50 +0200

    feat(pc-cleaner): add preferences for the checkboxes

--- src/Section10/PcCleaner/Views/MainPage.xaml.cs
+++ src/Section10/PcCleaner/Views/MainPage.xaml.cs
@@ -4,23 +4,43 @@ namespace PcCleaner.Views;
 
 public partial class MainPage : ContentPage
 {
-    private bool _temporaryFilesChecked = true;
-    private bool _binChecked = true;
-    private bool _logsChecked = true;
-    private bool _windowsUpdateChecked = true;
-    private bool _errorsChecked = true;
+    private bool _temporaryFilesChecked;
+    private bool _binChecked;
+    private bool _logsChecked;
+    private bool _windowsUpdateChecked;
+    private bool _errorsChecked;
 
     public MainPage()
     {
         InitializeComponent();
 
         ShowSystemInfo();
+
+        InitializeCheckBoxesStates();
     }
 
     public void ShowSystemInfo()
     {
         OsVersionLabel.Text = SystemInfo.GetWindowsVersion();
         HardwareLabel.Text = SystemInfo.GetHardwareInfo();
+    }
+
+    public void InitializeCheckBoxesStates()
+    {
+        _temporaryFilesChecked = Preferences.Get(nameof(_temporaryFilesChecked), false);
+        TemporaryFilesCheckBox.IsChecked = _temporaryFilesChecked;
+
+        _binChecked = Preferences.Get(nameof(_binChecked), true);
+        EmptyBinCheckBox.IsChecked = _binChecked;
+
+        _logsChecked = Preferences.Get(nameof(_logsChecked), true);
+        LogsCheckBox.IsChecked = _logsChecked;
+
+        _windowsUpdateChecked = Preferences.Get(nameof(_windowsUpdateChecked), true);
+        WindowsUpdateCheckBox.IsChecked = _windowsUpdateChecked;
+
+        _errorsChecked = Preferences.Get(nameof(_errorsChecked), true);
+        ErrorsCheckBox.IsChecked = _errorsChecked;
     }
 
     private async void OnInfoClicked(object? sender, EventArgs e)
@@ -49,25 +69,30 @@ public partial class MainPage : ContentPage
     private void OnTemporaryFilesCheckedChanged(object? sender, CheckedChangedEventArgs e)
     {
         _temporaryFilesChecked = e.Value;
+        Preferences.Set(nameof(_temporaryFilesChecked), _temporaryFilesChecked);
     }
 
     private void OnEmptyBinCheckedChanged(object? sender, CheckedChangedEventArgs e)
     {
         _binChecked = e.Value;
+        Preferences.Set(nameof(_binChecked), _binChecked);
     }
 
     private void OnLogsCheckedChanged(object? sender, CheckedChangedEventArgs e)
     {
         _logsChecked = e.Value;
+        Preferences.Set(nameof(_logsChecked), _logsChecked);
     }
 
     private void OnWindowsUpdateCheckedChanged(object? sender, CheckedChangedEventArgs e)
     {
         _windowsUpdateChecked = e.Value;
+        Preferences.Set(nameof(_windowsUpdateChecked), _windowsUpdateChecked);
     }
 
     private void OnErrorsCheckedChanged(object? sender, CheckedChangedEventArgs e)
     {
         _errorsChecked = e.Value;
+        Preferences.Set(nameof(_errorsChecked), _errorsChecked);
     }
 }
